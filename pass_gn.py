94ea15ba87a2e167aa9d6d943528f0df45f1581f
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-10-02 15:31:32 +0330

    yeeeeeeeh ! this is a first version .

--- pass_gn.py
+++ pass_gn.py
@@ -1,25 +1,23 @@
 # start >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
 # import Library -----------------------------------
 import random
 from colorama import init, Fore
-
 init()
 # login --------------------------------------------
-print(Fore.RED+'Welcome To your Password Generator !')
+print('Welcome To your Password Generator !')
 # chars --------------------------------------------
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?1234567890'
 # promts -------------------------------------------
 number = input('Amount of password to generate: ')
 number = int(number)
 
-length = input('input your password length: ')
+length = input(Fore.WHITE+'input your password length: ')
 length = int(length)
 # print status -------------------------------------
 print(Fore.WHITE+'\nHere are Your Passwords: ')
 # loop ---------------------------------------------
 for pwd in range(number):
-    passwords = Fore.GREEN+"#!#   " + ''
+    passwords = Fore.GREEN+" [+] " + Fore.WHITE+''
     for c in range(length):
         passwords += random.choice(chars)
     print(passwords)
